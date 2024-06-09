b6e2cf5b93dc5319f662d599b10ee6d376d67cd3
Carlos <carlosmgsv22@gmail.com>
2024-06-09 13:50:57 -0600

    modifications on playlist.php and styles.css

---
+++ playlist.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playlist</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <header><?php include('./view/header.php') ?></header>
+    <main>
+    <div class="flexPlaylist">
+        <div class="divPlaylist">
+        <a>Add songs</a>
+        </div>
+        <div class="divPlaylist">
+        <a>Manage your playlists</a>
+        </div>
+    </div>
+    </main>
+    <footer><?php include('./view/footer.php') ?></footer>
+</body>
+</html>
--- view/header.php
+++ view/header.php
@@ -1,10 +1,11 @@
+<header>
 <div class="header">
         <a href="home.php" class="logo">Kanye West</a>   
         <div class="header-right">
             <a class="login-name">Account</a>
             <a class="active" href="home.php">Home</a>
-            <a href="#playlist">Make a playlist</a>
-            <a href="#about">About</a>
-            <a href="#shoppingCart" class="cartAnchor"><img class="cartImg" src="./images/shopping-cart.png" style="width: 50px; height: 45px;"></a>
+            <a href="playlist.php">Make a playlist</a>
+            <a href="#shoppingCart" class="cartAnchor">Cart</a>
         </div>
     </div>
+</header>
